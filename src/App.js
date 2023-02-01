fe345d94e94ed56cb4ff1a3abbed664165ef708e
Nick Murphy <63930644+Murphyn5@users.noreply.github.com>
2023-01-31 16:55:12 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>App Component</h1>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+        <Route>'Page Not Found'</Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
